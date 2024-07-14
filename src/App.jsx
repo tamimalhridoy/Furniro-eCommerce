a0730085c7009c858378d3dd4d6654fe7b6a9d13
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-07-14 17:19:24 +0600

    navbar start

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+
 function App() {
-  return <div>hello</div>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/" element={<Layout />}>
+        <Route index element={<Home/>}></Route>
+        </Route>
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
