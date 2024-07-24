d3324c38d740dd10977607b8261581a9849d633e
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-07-25 00:00:35 +0600

    brw padding

--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -8,7 +8,11 @@ const Products = () => {
         <h2 className="font-primary text-[40px] font-bold text-[#3A3A3A] text-center">
           Our Products
         </h2>
-        <div className="grid grid-cols-4 gap-8">
+        <div className="grid grid-cols-4 gap-8 mt-11">
+          <ProductsLiat />
+          <ProductsLiat />
+          <ProductsLiat />
+          <ProductsLiat />
           <ProductsLiat />
           <ProductsLiat />
           <ProductsLiat />
