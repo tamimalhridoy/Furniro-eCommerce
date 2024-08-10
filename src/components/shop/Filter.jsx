a9d3a8e9db0ab03d0667bb6ea6a1fa67c6d7c4a7
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-08-10 18:54:59 +0600

    try

--- src/components/shop/Filter.jsx
+++ src/components/shop/Filter.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { HiAdjustmentsHorizontal } from "react-icons/hi2";
+import { BiGridSmall } from "react-icons/bi";
+import { MdOutlineViewDay } from "react-icons/md";
 
 const Filter = () => {
   return (
     <section className="py-10 bg-[#F9F1E7]">
-      <div className="container"></div>
+      <div className="container flex ">
+        <p className="font-primary text-xl items-center">
+          <HiAdjustmentsHorizontal />
+          <span>Filter</span>
+          <p className="">
+            <BiGridSmall className="text-4xl" />
+            <MdOutlineViewDay className="text-3xl" />
+          </p>
+          <p className="font-primary text-lg">Showing 1–16 of 32 results</p>
+        </p>
+      </div>
     </section>
   );
 };
