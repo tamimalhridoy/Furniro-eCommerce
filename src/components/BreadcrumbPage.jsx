4e49bf8be5c90e5d8f4158561eca417452ffeb22
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-08-20 23:21:09 +0600

    start

--- src/components/BreadcrumbPage.jsx
+++ src/components/BreadcrumbPage.jsx
@@ -4,17 +4,30 @@ import { FaChevronRight } from "react-icons/fa6";
 
 const BreadcrumbPage = () => {
   const location = useLocation();
-  const path = location.pathname.split("/").filter((item) => item !== "");
-  console.log(location)
+  const path1 = location.pathname.split("/")[1];
+  const path2 = location.pathname.split("/")[2];
+  console.log(path2);
   return (
-    <ul className=" flex text-center items-center gap-1 justify-center mt-2">
+    <ul className=" flex text-center items-center gap-5 justify-center mt-2">
       <li className="font-primary font-medium text-base text-[#]">
         <Link to="/">Home</Link>
       </li>
-      <FaChevronRight />
-      <li className="font-primary font-medium text-base text-[#000000e4]">
-        <p>{path}</p>
+      <li>
+        <FaChevronRight />
       </li>
+      <li className="font-primary font-medium text-base text-[#000000e4]">
+        <Link to="/shop">{path1}</Link>
+      </li>
+      {path2 && (
+        <>
+          <li className="pr-8">
+            <FaChevronRight />
+          </li>
+          <li className="font-primary font-medium text-base text-[#000000e4] pl-8 border-l-2 border-[#9F9F9F]">
+            <p>{path2}</p>
+          </li>
+        </>
+      )}
     </ul>
   );
 };
