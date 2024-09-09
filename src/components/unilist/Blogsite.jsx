14f17730dfe891877bb714abc3c2ded8f0a5b0d2
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-09-09 23:23:18 +0600

    about

--- src/components/unilist/Blogsite.jsx
+++ src/components/unilist/Blogsite.jsx
@@ -1,13 +1,56 @@
-import React from 'react'
+import React from "react";
+import { IoPerson } from "react-icons/io5";
+import { CgCalendarDates } from "react-icons/cg";
+import { FaTag } from "react-icons/fa";
 
 const Blogsite = () => {
   return (
     <section>
-        <div className="container">
-            
-        </div>
-    </section>
-  )
-}
+      <div className="container">
+        <div className="w-3/4 pt-24">
+          <div className="w-[817px] h-[500px]">
+            <img
+              src="/blogsite.png"
+              className="w-full rounded-lg"
+              alt="blogsite-1"
+            />
+          </div>
+          <div className="flex gap-9 py-4">
+            <div className="text-[#9F9F9F] flex items-center gap-2 font-primary text-base">
+              <IoPerson />
+              <p>Admin</p>
+            </div>
+            <div className="text-[#9F9F9F] flex items-center gap-2 font-primary text-base">
+              <CgCalendarDates />
+              <p>14 Oct 2022</p>
+            </div>
+            <div className="text-[#9F9F9F] flex items-center gap-2 font-primary text-base">
+              <FaTag />
+              <p>Wood</p>
+            </div>
+          </div>
+          <h2 className="font-primary font-medium text-3xl">
+            Going all-in with millennial design
+          </h2>
+          <p className="pt-3 pb-7 font-primary text-[#9F9F9F] w-[860px] text-base leading-[150%]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus
+            mauris vitae ultricies leo integer malesuada nunc. In nulla posuere
+            sollicitudin aliquam ultrices. Morbi blandit cursus risus at
+            ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in.
+            Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis
+            nunc sed blandit libero. Pellentesque elit ullamcorper dignissim
+            cras tincidunt. Pharetra et ultrices neque ornare aenean euismod
+            elementum.
+          </p>
+          <button>
 
-export default Blogsite
+            
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Blogsite;
