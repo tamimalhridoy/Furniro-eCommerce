d19ec54fb47ed1de6c10f95caa5ddf2032a82783
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-10-26 22:42:13 +0600

    vul commit

--- src/components/unilist/Input.jsx
+++ src/components/unilist/Input.jsx
@@ -12,6 +12,15 @@ const Input = ({ title, type = "text" }) => {
         className="border border-[#9F9F9F] py-3 rounded-lg px-3 w-full"
       />
     </div>
+    <div className="pt-9">
+    <label className="font-primary font-medium text-base pb-5 inline-block">
+      {title}
+    </label>
+    <input
+      type="{type}"
+      className="border border-[#9F9F9F] py-3 rounded-lg px-3 w-full"
+    />
+  </div>
   );
 };
 
