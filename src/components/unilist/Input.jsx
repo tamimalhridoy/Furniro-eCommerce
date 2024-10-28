eff1b81d08e225888623874e20d6407ccec3b4eb
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-10-28 16:27:08 +0600

    try

--- src/components/unilist/Input.jsx
+++ src/components/unilist/Input.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 const Input = ({ title, type = "text" }) => {
   return (
     <div className="pt-9">
@@ -12,15 +11,6 @@ const Input = ({ title, type = "text" }) => {
         className="border border-[#9F9F9F] py-3 rounded-lg px-3 w-full"
       />
     </div>
-    <div className="pt-9">
-    <label className="font-primary font-medium text-base pb-5 inline-block">
-      {title}
-    </label>
-    <input
-      type="{type}"
-      className="border border-[#9F9F9F] py-3 rounded-lg px-3 w-full"
-    />
-  </div>
   );
 };
 
